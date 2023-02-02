c4f38eb2cb52df92bd6f7d74d14664a7179cb889
Samrat Chakraborty <98039377+Samrat1507@users.noreply.github.com>
2023-02-02 15:27:09 +0530

    Create detect_capital.cpp

---
+++ detect_capital.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+bool detectCapitalUse(string word)
+{
+int l=word.length();
+
+    int cap=0;int lower=0;
+    for(int i=0;i<l;i++)
+    {
+        if(islower(word[i]))    lower++;
+        else cap++;
+
+    }
+    if(cap==l||lower==l||(isupper(word[0])&&lower==l-1))
+        return true;
+    else    return false;
+    
+}
+};
